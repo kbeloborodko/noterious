b1552b841b6753c51a9ded085a97d322d684b572
kbeloborodko <kbeloborodko@gmail.com>
2017-02-13 15:27:08 +0200

    Initial project structure upload

---
+++ src/app/noterious.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('noterious', [
+  'ui.router',
+  'ngAnimate',
+  'firebase',
+  'noterious.common'
+])
+  .constant('ENDPOINT_URI', 'https://noterious.firebaseio.com/')
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/login');
+
+    $stateProvider
+      .state('login', {
+        url:'/login',
+        templateUrl: 'app/login/login.tmpl.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+    ;
+
+    /* HINT: Add this to your boards route to force authentication
+     resolve: {
+       'currentUser': ['Auth', function (Auth) {
+        return Auth.$requireAuth();
+       }]
+     }
+     */
+  })
+  .run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      event.preventDefault();
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('login');
+      }
+    });
+  })
+;
